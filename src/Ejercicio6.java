a04c6e20c933d885bb6988152188f50b159a8988
williamernestoojedagomez4-cmyk <williamernestoojedagomez4@gmail.com>
2026-05-18 23:56:25 -0600

    Ejercicio 6
    Ejercicio finalizado

---
+++ src/Ejercicio6.java
@@ -0,0 +1,17 @@
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        System.out.println("--- Primeros 50 múltiplos de 5 ---");
+
+        // Ciclo for
+        for (int i = 1; i <= 50; i++) {
+
+            // Calculamos el múltiplo de 5
+            int multiplo = i * 5;
+
+            // Mostramos el resultado
+            System.out.println(multiplo);
+        }
+    }
+}
